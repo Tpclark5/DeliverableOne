d53206ad5916371b1cd14f9cf1858b603791afef
Tim <TimothyClark@nexsystech.com>
2019-12-09 00:18:29 -0500

    Commit for deliverable one

---
+++ ConsoleApplication3/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            {
+                string destination = "";
+                string transportType = "";
+                Console.Write("What type of vacation would you like to take, musical, tropical, or adventurous: ");
+                string vacation = Console.ReadLine();
+                Console.Write("How many are in your group: ");
+                int groupSize = int.Parse(Console.ReadLine());
+
+                {
+
+
+                    if (vacation == "musical")
+                    {
+                       destination = ("New Orleans");
+                    }
+                    else if (vacation == "tropical")
+                    {
+                        destination = ("a beach vacation in Mexico");
+                    }
+                    else if (vacation == "adventurous")
+                    {
+                        destination = ("Whitewater rafting in the Grand Canyon");
+                    }
+
+                     if (groupSize <= 2)
+                    {
+                        transportType = "a first class flight";
+                            }
+                    else if (groupSize <= 5)
+                    {
+                        transportType = "a helicopter";
+                            }
+                    else if (groupSize >= 6)
+                    {
+                        transportType = ("a charter flight");
+                            }
+                    string results = ("Since you're a group of " + groupSize + " going on a " + vacation + " vacation, you should take " + transportType + " to " + destination + ".");
+                    Console.WriteLine(results);
+
+                }
+            }
+        }
+    }
+}
+    
+
